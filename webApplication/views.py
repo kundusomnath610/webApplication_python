f602d5358ab0482c14770b66e7c683eda10f5a48
Somnath Kundu <kundusomnath610@gmail.com>
2025-07-26 13:05:30 +0530

    commit

--- webApplication/views.py
+++ webApplication/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+"""
+    render needs two parameter resuest and what page we want to render
+"""
+def home(resuest):
+    return render(resuest, "index.html") 
 
 def aboutUs(resuest):
     return HttpResponse("Welcome the page")
